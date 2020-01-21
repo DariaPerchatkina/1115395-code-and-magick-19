01239db6279380e5c51d45328a96f435b3d469c0
Дарья <perchatkinadaria@gmail.com>
2020-01-21 22:00:38 +0300

    удаление лишних пробелов

--- js/stat.js
+++ js/stat.js
@@ -3,12 +3,12 @@
 var CLOUD_WIDTH = 270;
 var CLOUD_HEIGHT = 420;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx) {
+window.renderStatistics = function (ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 270, 420);
 
