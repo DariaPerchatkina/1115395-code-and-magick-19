2be0138798ac398b0b606d247d5c94f809c2aba9
Дарья <perchatkinadaria@gmail.com>
2020-02-11 00:25:13 +0400

    добавляет массивы данных для описания магов

--- js/setup.js
+++ js/setup.js
@@ -9,15 +9,19 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+.content
+.querySelector('.setup-similar-item');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_FAMILYS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210 ,55)', 'rgb(0, 0 ,0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [
   {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
+    name: WIZARD_NAMES[0] + WIZARD_FAMILYS[0],
+    coatColor: COAT_COLORS[0],
+    eyesColor: EYES_COLORS[0]
   },
   {
     name: WIZARD_NAMES[1],
@@ -38,6 +42,7 @@ var renderWizard = function (wizard) {
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
@@ -51,6 +56,3 @@ similarListElement.appendChild(fragment);
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 
-// var WIZARD_FAMILY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-// var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210 ,55)', 'rgb(0, 0 ,0)'];
-// var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
