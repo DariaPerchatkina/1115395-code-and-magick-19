9385e8ec3298578212ca7b6e0382e897976ca56a
Дарья <perchatkinadaria@gmail.com>
2020-02-11 00:00:50 +0400

    показываем блок с похожими персонажами, находим шаблон для копирования, находим элемент в который будут вставляться маги

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,13 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
 // var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 // var WIZARD_FAMILY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 // var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210 ,55)', 'rgb(0, 0 ,0)'];
